a15156de1c390427f4d48bc5277da6abb5ebab97
Matthew Walker <mwalker176@gmail.com>
2021-09-19 03:53:10 -0600

    Started work on a render settings class

---
+++ mre/src/Rendering/RenderSettings.h
@@ -0,0 +1,24 @@
+#ifndef RENDERSETTINGS_H
+#define RENDERSETTINGS_H
+
+
+class RenderSettings {
+public:
+
+
+private:
+	//use multithreading
+	//use supersampling
+	//subrows
+	//subcolumns
+	//max trace depth
+	//max samples
+
+};
+
+
+
+
+
+
+#endif
