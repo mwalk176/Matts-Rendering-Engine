c0d0bf908851a6afdcab76feef957d3c20a5e180
Matthew Walker <mwalker176@gmail.com>
2021-09-21 09:44:21 -0600

    Added getMaterial() function that returns the primary material

--- mre/src/Scene/SceneObjects/SceneObject.cpp
+++ mre/src/Scene/SceneObjects/SceneObject.cpp
@@ -1,12 +1,33 @@
 #include "SceneObject.h"
 
+//Material* SceneObject::primaryMaterial;
+
 SceneObject::SceneObject() {
     materials.push_back(new MDebugMat());
+    primaryMaterial = nullptr;
     //materials.push_back(new MRayTracerMat());
 }
 
 Vec3 SceneObject::getPos() {
     return pos;
+}
+
+Material* SceneObject::getMaterial(std::string mType) {
+    if (primaryMaterial == nullptr) {
+        for (int i = 0; i < materials.size(); i++) {
+            if (materials.at(i)->toString() == mType) {
+                primaryMaterial = materials.at(i);
+                return materials.at(i);
+            }
+        }
+        return nullptr;
+    }
+    return primaryMaterial;
+
+}
+
+void SceneObject::setPrimaryMaterial(Material* m) {
+    primaryMaterial = m;
 }
 
 std::vector<Material*> SceneObject::getMaterials() {
--- mre/src/Scene/SceneObjects/SceneObject.h
+++ mre/src/Scene/SceneObjects/SceneObject.h
@@ -1,6 +1,8 @@
 #ifndef SCENEOBJECT_H
 #define SCENEOBJECT_H
 
+#define _USE_MATH_DEFINES
+
 #include "../../Utils/Vec3.h"
 #include "../../Utils/Ray.h"
 #include "../../Materials/Material.h"
@@ -13,6 +15,9 @@ public:
 	Vec3 getPos();
 	virtual bool intersect(Ray r, double& p0, double& p1) = 0;
 	virtual Vec3 computeNormal(Vec3 intersectionPoint) = 0;
+	virtual Vec3 getNewDirectionTowardsLight(Vec3 shadowRay, Vec3 alignedNormal, Vec3 normalFromLight, double& angleToObject, Vec3 intersectionPoint) = 0;
+	Material* getMaterial(std::string mType);
+	void setPrimaryMaterial(Material* m);
 
 	std::vector<Material*> getMaterials();
 
@@ -22,6 +27,7 @@ public:
 protected:
 	Vec3 pos;
 	std::vector<Material*> materials;
+	Material* primaryMaterial;
 
 private:
 
